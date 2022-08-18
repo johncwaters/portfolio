7fec7c5ec3765b287aafbc842081ddd09aa8f85d
OtzoLive <john@johncwaters.com>
2022-08-17 21:58:51 -0600

    Feat: blog back button and back to top button

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,6 +13,6 @@ export default defineConfig({
 	adapter: vercel(),
 	integrations: [image(), sitemap(), svelte(), tailwind()],
 	daisyui: {
-		themes: ["light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"],
+		themes: ["wireframe"],
 	},
 });
