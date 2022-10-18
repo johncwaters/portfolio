f74f88915a3f8bf85ab39751593b322b9666f51d
OtzoLive <john@johncwaters.com>
2022-10-17 20:03:49 -0600

    revert

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,13 @@ import image from '@astrojs/image';
 import svelte from '@astrojs/svelte';
 import tailwind from '@astrojs/tailwind';
 
-import vercel from '@astrojs/vercel/static';
+import vercel from '@astrojs/vercel/serverless';
 
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	output: 'static',
+	output: 'server',
 	adapter: vercel(),
 	integrations: [image(), sitemap(), svelte(), tailwind()],
 	site: 'https://johncwaters.com',
