aa5fce7954c0e04d6f5ceffba0576af9f002f223
Spy1201 <john@johncwaters.com>
2022-11-24 07:05:09 -0700

    convert to netlify hosting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,15 @@
 import { defineConfig } from 'astro/config';
 import image from '@astrojs/image';
-import svelte from '@astrojs/svelte';
 import tailwind from '@astrojs/tailwind';
-
-import vercel from '@astrojs/vercel/serverless';
-
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
+import netlify from "@astrojs/netlify/functions";
+
+// https://astro.build/config
 export default defineConfig({
-	output: 'server',
-	adapter: vercel(),
-	integrations: [image(), sitemap(), svelte(), tailwind()],
-	site: 'https://johncwaters.com',
+  output: 'server',
+  adapter: netlify(),
+  integrations: [image(), sitemap(), svelte(), tailwind()],
+  site: 'https://johncwaters.com'
 });
