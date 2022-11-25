e5ed6f434c5d07b6a7eea3aabd1e99f3774445bf
Spy1201 <john@johncwaters.com>
2022-11-24 22:24:54 -0700

    adjusted header navbar

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,10 @@
 module.exports = {
-    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-    theme: {
-        extend: {},
-    },
-    plugins: [require("daisyui"), require('@tailwindcss/typography')],
-    daisyui: {
-        themes: ["wireframe"],
-    },
-};
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {}
+  },
+  plugins: [require('daisyui'), require('@tailwindcss/typography')],
+  daisyui: {
+    themes: ['lofi']
+  }
+}
