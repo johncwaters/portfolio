65a00d4b1e0094cc4f8868d6b01a13123182399b
John C. Waters <john@johncwaters.com>
2023-09-28 19:59:11 -0600

    swap to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import netlify from "@astrojs/netlify/functions";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: netlify(),
+  adapter: vercel(),
   integrations: [tailwind()],
   site: "https://johncwaters.com",
 });
