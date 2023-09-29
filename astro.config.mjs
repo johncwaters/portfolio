dba033cbbe0b48dc010c4d8d18b1c72f0b0667c1
John C. Waters <john@johncwaters.com>
2023-09-28 19:59:28 -0600

    enable webAnalytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,11 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel(),
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
   integrations: [tailwind()],
   site: "https://johncwaters.com",
 });
